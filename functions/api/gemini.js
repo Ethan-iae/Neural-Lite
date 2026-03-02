5c9945748c4cd2fe55a0974b79dde55d2953be16
Ethan <123637351+Ethan-iae@users.noreply.github.com>
2026-03-02 16:17:28 +0800

    Add files via upload

--- functions/api/gemini.js
+++ functions/api/gemini.js
@@ -105,7 +105,7 @@ export async function onRequestPost(context) {
     });
 
     // 4. 请求 Google Gemini API
-    const targetUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
+    const targetUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${apiKey}`;
 
     try {
         const geminiRes = await fetch(targetUrl, {
